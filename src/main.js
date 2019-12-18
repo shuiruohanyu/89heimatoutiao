19b2b8805219e353e10c4d00a0e1059f73bf639d
gaoly <shuiruohanyu@foxmail.com>
2019-12-18 10:22:33 +0800

    黑马头条PC-初始化项目-安装ElementUI

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui' // 引入包
+import 'element-ui/lib/theme-chalk/index.css' // 引入elemenUI样式
 import './styles/index.less' // 引入初始化样式
 Vue.config.productionTip = false
-
+Vue.use(ElementUI) // 全局注册
 new Vue({
   router,
   render: h => h(App)
