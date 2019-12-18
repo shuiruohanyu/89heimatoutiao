38c9efe9a37964217c3316d6da1a26c1cca1dd5a
gaoly <shuiruohanyu@foxmail.com>
2019-12-18 12:21:21 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,23 +5,24 @@
        <div class='title'>
          <img src="../../assets/img/logo_index.png" alt="">
        </div>
-       <!-- 登录表单 表单容器 -->
-       <el-form style="margin-top:30px">
-         <!-- 表单域 el-form-item => 一行 -->
-         <el-form-item>
-           <!-- 放置具体的组件 登录手机号 -->
-           <el-input placeHolder="请输入手机号"></el-input>
+       <!-- 登录表单 表单容器  el-form 需要绑定model属性 rules属性绑定验证规则对象-->
+       <el-form ref="myForm" style="margin-top:30px" :model="loginForm" :rules="loginRules">
+         <!-- 表单域 el-form-item => 一行   => 校验 => prop => 要检验的字段名-->
+         <el-form-item prop="mobile">
+           <!-- 放置具体的组件 登录手机号 v-model 双向绑定数据对象-->
+           <el-input v-model="loginForm.mobile" placeHolder="请输入手机号"></el-input>
          </el-form-item>
-         <el-form-item>
+         <el-form-item prop="code">
            <!-- 表单域 -->
-           <el-input style="width:65%" placeHolder="请输入验证码"> </el-input>
+           <el-input v-model="loginForm.code" style="width:65%" placeHolder="请输入验证码"> </el-input>
            <el-button style="float:right" plain>发送验证码</el-button>
          </el-form-item>
-         <el-form-item>
-           <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+         <el-form-item prop="check">
+           <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
          </el-form-item>
          <el-form-item>
-           <el-button type="primary" style="width:100%">登录</el-button>
+           <!-- 注册点击事件 -->
+           <el-button @click="submitLogin" type="primary" style="width:100%">登录</el-button>
          </el-form-item>
        </el-form>
      </el-card>
@@ -30,7 +31,52 @@
 
 <script>
 export default {
-
+// 第一步 在data中定义表单数据对象
+  data () {
+    return {
+      // 定义一个表单数据对象
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        check: false // 是否勾选
+      },
+      loginRules: {
+        // 验证规则 验证登录表单的  key(字段名):value(数组)
+        // required true -> 必填
+        mobile: [{ required: true, message: '请输入您的手机号' }, {
+          pattern: /^1[3456789]\d{9}$/, message: '手机号格式不正确'
+        }],
+        code: [{ required: true, message: '请输入您的验证码' }, {
+          pattern: /^\d{6}$/,
+          message: '验证码格式不正确'
+        }],
+        // 自定义函数
+        check: [{ validator: function (rule, value, callback) {
+          //   rule当前的规则 没什么用
+          // value指的就是我们要校验的字段的值
+          if (value) {
+            //  认为校验通过 放过去
+            callback() // 直接执行callback 认为通过
+          } else {
+            //  认为校验不通过 要提示信息
+            callback(new Error('您必须无条件同意被我们坑'))
+          }
+        } }]
+      }
+    }
+  },
+  methods: {
+    // 提交登录表单
+    submitLogin () {
+      //  el-form实例
+      this.$refs.myForm.validate(function (isOK) {
+        if (isOK) {
+          //  认为前端校验登录表单成功
+          console.log('前端校验成功,发送用户名和密码到后台去校验')
+        }
+      })
+    }
+  }
 }
 </script>
 
