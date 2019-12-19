679b8660b05afec5ad4d94e385cde10dfb9b778f
gaoly <shuiruohanyu@foxmail.com>
2019-12-19 09:11:35 +0800

    黑马头条PC-登录模块-安装axios-配置全局使用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import ElementUI from 'element-ui' // 引入包
 import 'element-ui/lib/theme-chalk/index.css' // 引入elemenUI样式
 import './styles/index.less' // 引入初始化样式
+import axios from 'axios'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 赋值黑马头条的默认地址
+Vue.prototype.$axios = axios // 赋值给全局对象
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册
 new Vue({
