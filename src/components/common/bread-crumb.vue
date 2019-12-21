41089a081341baff7abc5c90f031fd668789c124
gaoly <shuiruohanyu@foxmail.com>
2019-12-21 11:55:19 +0800

    黑马头条PC-评论列表-页面结构-table

--- src/components/common/bread-crumb.vue
+++ src/components/common/bread-crumb.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- 面包屑的内容 -->
-  <el-breadcrumb>
+  <el-breadcrumb separator=">">
       <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
       <el-breadcrumb-item>
          <!-- 具名插槽 -->
--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>评论列表</div>
+<!-- 卡片组件 -->
+  <el-card>
+    <!-- 面包屑给了卡片的具名插槽 -->
+       <bread-crumb slot="header">
+          <!-- 插槽内容 -->
+         <template slot="title">评论列表</template>
+       </bread-crumb>
+       <!-- body内容 -->
+       <el-table>
+         <!-- 列组件 label 表头-->
+         <el-table-column  width="600" label="标题"></el-table-column>
+         <el-table-column label="评论状态"></el-table-column>
+         <el-table-column label="总评论数"></el-table-column>
+         <el-table-column label="粉丝评论数"></el-table-column>
+         <el-table-column label="操作"></el-table-column>
+       </el-table>
+  </el-card>
 </template>
 
 <script>
