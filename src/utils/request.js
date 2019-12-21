8b9b66255115e3355d517538246494c59d657e3a
gaoly <shuiruohanyu@foxmail.com>
2019-12-21 11:04:45 +0800

    黑马头条PC--接口访问-axios拦截器-统一处理异常数据

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,7 @@
 // 对axios进行一个封装
 import axios from 'axios'
+import router from '../router' // 路由实例对象引入
+import { Message } from 'element-ui' // 引入提示对象
 // 请求拦截器
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 赋值黑马头条的默认地址
 // 请求拦截
@@ -16,7 +18,38 @@ axios.interceptors.request.use(function (config) {
 axios.interceptors.response.use(function (response) {
   // 成功时执行
   return response.data ? response.data : {} // 解决当data不存在时  then中读取数据报错问题
-}, function () {
+}, function (error) {
+  console.log(error)
+  // 回调函数  所有的失败 都会进入到第二个回调函数
   // 失败时执行 状态码 不是200 或者201/204
+  // 获取状态码 => 根据状态码 进行相应的提示或者操作
+  let status = error.response.status // 获取状态码  根据状态码进行提示
+  // let configurl = error.config.url // 请求地址
+  let message = '' // 提示信息
+  switch (status) {
+    case 400:
+      message = '手机号或者验证码错误'
+      break
+    case 403:
+    // 如果同样的状态码 但是不同意思 => 需要通过 请求地址来判断是哪个的响应  请求地址 + 状态码 一起来判断 怎么处理
+      // resfehtoken 过期 强制跳转到登录页 resfehtoken => 是用来换取 token的
+      // this.$router => 路由实例对象
+      router.push('/login')
+      break
+    case 401:
+      // token过期
+      router.push('/login') // 强制回登录
+      break
+    case 507:
+      message = '服务器数据库异常'
+      break
+    case 404:
+      message = '手机号不正确'
+      break
+    default:
+      break
+  }
+  Message({ type: 'warning', message }) // 提示消息
+  // 状态码 提示
 })
 export default axios
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -82,12 +82,6 @@ export default {
             window.localStorage.setItem('user-token', result.data.token) // 前端缓存令牌
             this.$router.push('/home') // 跳转到主页
             //  成功以后才会进入到then
-          }).catch(() => {
-            // elementUI的方法
-            this.$message({
-              message: '您的手机号或者验证码不正确',
-              type: 'warning'
-            })
           })
         }
       })
