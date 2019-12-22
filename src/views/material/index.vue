513a3cb4ae56b82f3fca7d5ec9ea9d655185a914
gaoly <shuiruohanyu@foxmail.com>
2019-12-22 11:04:17 +0800

    黑马头条PC-素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -4,12 +4,30 @@
       <bread-crumb slot="header">
          <template slot="title">素材管理</template>
       </bread-crumb>
+      <!-- 素材 -->
+      <el-tabs v-model="activeName">
+          <el-tab-pane label="全部素材" name="all">
+              <!-- 全部素材内容 -->
+
+          </el-tab-pane>
+          <el-tab-pane label="收藏素材" name="collect">
+              <!-- 收藏素材内容 -->
+          </el-tab-pane>
+
+      </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      activeName: 'collect'
+    }
+  },
+  methods: {
 
+  }
 }
 </script>
 
