da3986b42f464795f3de304d5be1c52781eee3e9
gaoly <shuiruohanyu@foxmail.com>
2019-12-22 12:11:42 +0800

    黑马头条PC-素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,35 +1,54 @@
 <template>
-<!-- 基本的页面结构 -->
+  <!-- 基本的页面结构 -->
   <el-card>
-      <bread-crumb slot="header">
-         <template slot="title">素材管理</template>
-      </bread-crumb>
-      <!-- 素材 -->
-      <el-tabs v-model="activeName" @tab-click="changeTab">
-          <el-tab-pane label="全部素材" name="all">
-              <!-- 全部素材内容 定制 -->
-              <div class="img-list">
-                <!-- v-for -->
-                 <el-card class='img-card' v-for="item in list" :key="item.id">
-                     <img :src="item.url" alt="">
-                    <el-row class='operate' type='flex' align="middle" justify="space-around" >
-                        <i class='el-icon-star-on'></i>
-                        <i class='el-icon-delete-solid'></i>
-                    </el-row>
-                 </el-card>
-              </div>
-          </el-tab-pane>
-          <el-tab-pane label="收藏素材" name="collect">
-              <!-- 收藏素材内容 -->
-             <div class="img-list">
-                <!-- v-for -->
-                 <el-card class='img-card' v-for="item in list" :key="item.id">
-                     <img :src="item.url" alt="">
-                 </el-card>
-              </div>
-          </el-tab-pane>
-
-      </el-tabs>
+    <bread-crumb slot="header">
+      <template slot="title">素材管理</template>
+    </bread-crumb>
+    <!-- 素材 -->
+    <el-tabs v-model="activeName" @tab-click="changeTab">
+      <el-tab-pane label="全部素材" name="all">
+        <!-- 全部素材内容 定制 -->
+        <div class="img-list">
+          <!-- v-for -->
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+            <el-row class="operate" type="flex" align="middle" justify="space-around">
+              <i class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
+            </el-row>
+          </el-card>
+        </div>
+        <el-row type="flex" justify="center" style="height:80px" align="middle">
+          <el-pagination
+            background
+            layout="prev, pager, next"
+            :total="page.total"
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+            @current-change="changePage"
+          ></el-pagination>
+        </el-row>
+      </el-tab-pane>
+      <el-tab-pane label="收藏素材" name="collect">
+        <!-- 收藏素材内容 -->
+        <div class="img-list">
+          <!-- v-for -->
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+          </el-card>
+        </div>
+        <el-row type="flex" justify="center" style="height:80px" align="middle">
+          <el-pagination
+            background
+            layout="prev, pager, next"
+            :total="page.total"
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+            @current-change="changePage"
+          ></el-pagination>
+        </el-row>
+      </el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
@@ -38,12 +57,23 @@ export default {
   data () {
     return {
       activeName: 'all', // 默认选中全部
-      list: [] // 接收全部数据
+      list: [], // 接收全部数据
+      page: {
+        currentPage: 1, // 当前页码
+        pageSize: 8, // 每页多少条
+        total: 0 // 总条数
+      }
     }
   },
   methods: {
+    //   切换分页
+    changePage (newPage) {
+      this.page.currentPage = newPage // 得到最新页码
+      this.getAllMaterial()
+    },
     //   切换tab事件
     changeTab () {
+      this.page.currentPage = 1 // 应该把当前页码回到第一页 如果不重置第一页 就会直接去找不到对应页码
       this.getAllMaterial()
     },
     //   获取所有素材/收藏
@@ -51,9 +81,14 @@ export default {
       // all 所有 collect  收藏  this.activeName === "collect"
       this.$axios({
         url: '/user/images',
-        params: { collect: this.activeName === 'collect' }
+        params: {
+          collect: this.activeName === 'collect',
+          page: this.page.currentPage,
+          per_page: this.page.pageSize
+        }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count //
       })
     }
   },
@@ -64,27 +99,27 @@ export default {
 </script>
 
 <style lang='less' scoped>
- .img-list {
-     display: flex;
-     flex-wrap: wrap;
-     .img-card {
-         width: 200px;
-         height:240px;
-         margin: 20px 50px;
-         position: relative;
-         img {
-             width: 100%;
-             height: 100%;
-         }
-         .operate {
-             width: 100%;
-             font-size:20px;
-             position: absolute;
-             bottom: 0;
-             left:0;
-             background-color: #f4f5f6;
-             height:30px;
-         }
-     }
- }
+.img-list {
+  display: flex;
+  flex-wrap: wrap;
+  .img-card {
+    width: 200px;
+    height: 240px;
+    margin: 20px 50px;
+    position: relative;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .operate {
+      width: 100%;
+      font-size: 20px;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      background-color: #f4f5f6;
+      height: 30px;
+    }
+  }
+}
 </style>
