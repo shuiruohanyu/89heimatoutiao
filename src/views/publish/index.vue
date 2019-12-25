25b2501b340bd71a790c396363953b373a0a7e3f
gaoly <shuiruohanyu@foxmail.com>
2019-12-25 12:21:32 +0800

    黑马头条PC-发布文章-封面功能设计

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -9,9 +9,9 @@
           <el-input v-model="formData.title" style="width:60%"></el-input>
         </el-form-item>
         <el-form-item  prop="content" label="内容">
-          <quill-editor style="height:400px;" v-model="formData.content" type='textarea' :rows="4"></quill-editor>
+          <quill-editor style="height:400px;" v-model="formData.content"  ></quill-editor>
         </el-form-item>
-        <el-form-item prop="type" label="封面" style="margin-top:100px">
+        <el-form-item label="封面" style="margin-top:100px">
           <!-- 单选组  v-model="封面类型" -->
           <el-radio-group v-model="formData.cover.type">
             <el-radio :label="1">单图</el-radio>
@@ -74,6 +74,9 @@ export default {
           }
         }
       }
+    },
+    'formData.cover.type': function () {
+      debugger
     }
   },
   methods: {
