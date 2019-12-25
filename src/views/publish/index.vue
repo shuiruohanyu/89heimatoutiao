9c1335a64e5731dcf7dd75eb337de81c890a3e74
gaoly <shuiruohanyu@foxmail.com>
2019-12-25 11:21:59 +0800

    黑马头条PC-发表文章-修改文章-提交数据修改

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -89,22 +89,55 @@ export default {
     publishArticle (draft) {
       this.$refs.publishForm.validate((isOK) => {
         if (isOK) {
-          // 可以去进行 发布接口调用
+          let { articleId } = this.$route.params // 回去动态路由参数 articleId已经是字符串
           this.$axios({
-            url: '/articles',
-            method: 'post',
+            method: articleId ? 'put' : 'post',
+            url: articleId ? `/articles/${articleId}` : `/articles`,
             params: { draft }, // query参数
             data: this.formData
-          }).then(() => {
-            // 新增成功 => 应该去内容列表
+          }).then(result => {
             this.$router.push('/home/articles') // 回到内容列表
           })
+          // if (articleId) {
+          //   // 修改文章
+          //   this.$axios({
+          //     method: 'put',
+          //     url: `/articles/${articleId}`,
+          //     params: { draft }, // query参数
+          //     data: this.formData
+          //   }).then(() => {
+          //   // 新增成功 => 应该去内容列表
+          //     this.$router.push('/home/articles') // 回到内容列表
+          //   })
+          // } else {
+          // // 可以去进行 发布接口调用
+          //   this.$axios({
+          //     url: '/articles',
+          //     method: 'post',
+          //     params: { draft }, // query参数
+          //     data: this.formData
+          //   }).then(() => {
+          //   // 新增成功 => 应该去内容列表
+          //     this.$router.push('/home/articles') // 回到内容列表
+          //   })
+          // }
         }
+      })
+    },
+    // 获取文章详情通过id
+    getArticleById (articleId) {
+      this.$axios({
+        url: `/articles/${articleId}`
+      }).then(result => {
+        this.formData = result.data // 将指定文章数据给data数据
       })
     }
   },
   created () {
     this.getChannels() // 获取频道数据
+    // 获取id 判断有无id  有id 就是修改 没id就是发布
+    let { articleId } = this.$route.params // 回去动态路由参数 articleId已经是字符串
+    articleId && this.getArticleById(articleId) // 获取文章数据
   }
 }
 </script>
