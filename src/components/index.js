9c24feabcdc71ca925d35cf7e366bd5946766e69
gaoly <shuiruohanyu@foxmail.com>
2019-12-25 11:45:13 +0800

    黑马头条PC-发布文章-注册富文本编辑器并使用

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,15 @@
 import LayoutAside from './home/layout-aside'
 import LayoutHeader from './home/layout-header'
 import BreadCrumb from './common/bread-crumb'
+import { quillEditor } from 'vue-quill-editor' // 引入了quill组件
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 export default {
   install (Vue) {
     Vue.component('layout-aside', LayoutAside) // 全局注册
     Vue.component('layout-header', LayoutHeader) // 全局注册
     Vue.component('bread-crumb', BreadCrumb) // 全局注册一个面包屑组件
+    Vue.component('quill-editor', quillEditor) // 全局注册富文本编辑器
   }
 }
