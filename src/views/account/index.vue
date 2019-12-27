e82bc6704ea340b7bd8423da6fc8e77f155ee1b1
gaoly <shuiruohanyu@foxmail.com>
2019-12-27 11:33:37 +0800

    黑马头条PC-账户信息-用户数据保存

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -8,21 +8,21 @@
           <img :src="formData.photo ? formData.photo : defaultImg" alt="">
       </el-upload>
       <!-- 放置组件 -->
-     <el-form style="margin-left:100px" label-width="100px">
-         <el-form-item label="用户名">
+     <el-form ref="myForm" :model="formData" :rules="rules" style="margin-left:100px" label-width="100px">
+         <el-form-item label="用户名" prop="name">
              <el-input v-model="formData.name" style="width:30%"></el-input>
          </el-form-item>
-         <el-form-item label="简介">
+         <el-form-item label="简介" prop="intro">
               <el-input v-model="formData.intro"  style="width:30%"></el-input>
          </el-form-item>
-         <el-form-item label="邮箱">
+         <el-form-item label="邮箱" prop="email">
               <el-input v-model="formData.email"  style="width:30%"></el-input>
          </el-form-item>
          <el-form-item  label="手机号">
               <el-input v-model="formData.mobile" disabled style="width:30%"></el-input>
              </el-form-item>
              <el-form-item>
-                 <el-button type="primary">保存信息</el-button>
+                 <el-button @click="saveUserInfo" type="primary">保存信息</el-button>
             </el-form-item>
      </el-form>
   </el-card>
@@ -40,6 +40,18 @@ export default {
         email: '', // 邮箱
         mobile: '' // 手机号
       },
+      rules: {
+        name: [{ required: true, message: '用户名不能为空' }, {
+          min: 1,
+          max: 7,
+          message: '用户名的长度限制为1到7个字符'
+        }],
+        email: [{ required: true, message: '邮箱不能为空' },
+          {
+            pattern: /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/,
+            message: '邮箱格式不正确'
+          }]
+      }, // 定义一个规则
       defaultImg: require('../../assets/img/head.jpeg')
     }
   },
@@ -50,6 +62,27 @@ export default {
         url: '/user/profile'
       }).then(result => {
         this.formData = result.data
+      })
+    },
+    // baocunyonghuxin
+    // sucaiGuanli
+    saveUserInfo () {
+      // 要去校验表单数据是否OK
+      this.$refs.myForm.validate((isOK) => {
+        //   调用保存方法
+        if (isOK) {
+          this.$axios({
+            url: '/user/profile',
+            method: 'patch',
+            data: this.formData
+          }).then(result => {
+            //   认为保存成功
+            this.$message({
+              type: 'success',
+              message: '保存信息成功'
+            })
+          })
+        }
       })
     }
   },
