51f66ae813b0f976419ef862c8e68cc9782e2418
gaoly <shuiruohanyu@foxmail.com>
2019-12-27 11:13:45 +0800

    黑马头条PC-账户信息-数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -5,21 +5,21 @@
       </bread-crumb>
       <!-- 放置上传组件 -->
       <el-upload class='head-upload' action="" :show-file-list="false">
-          <img src="../../assets/img/head.jpeg" alt="">
+          <img :src="formData.photo ? formData.photo : defaultImg" alt="">
       </el-upload>
       <!-- 放置组件 -->
      <el-form style="margin-left:100px" label-width="100px">
          <el-form-item label="用户名">
-             <el-input style="width:30%"></el-input>
+             <el-input v-model="formData.name" style="width:30%"></el-input>
          </el-form-item>
          <el-form-item label="简介">
-              <el-input style="width:30%"></el-input>
+              <el-input v-model="formData.intro"  style="width:30%"></el-input>
          </el-form-item>
          <el-form-item label="邮箱">
-              <el-input style="width:30%"></el-input>
+              <el-input v-model="formData.email"  style="width:30%"></el-input>
          </el-form-item>
          <el-form-item  label="手机号">
-              <el-input disabled style="width:30%"></el-input>
+              <el-input v-model="formData.mobile" disabled style="width:30%"></el-input>
              </el-form-item>
              <el-form-item>
                  <el-button type="primary">保存信息</el-button>
@@ -30,7 +30,32 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      // 定义一个表单数据对象
+      formData: {
+        name: '', // 用户名
+        intro: '', // 简介
+        photo: '', // 头像
+        email: '', // 邮箱
+        mobile: '' // 手机号
+      },
+      defaultImg: require('../../assets/img/head.jpeg')
+    }
+  },
+  methods: {
+    //   获取用户信息
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(result => {
+        this.formData = result.data
+      })
+    }
+  },
+  created () {
+    this.getUserInfo() // 用户个人信息
+  }
 }
 </script>
 
